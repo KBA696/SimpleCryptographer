304af448b64f2442eb4a88403bdcf8b7e34d1e1d
Виталий <kba696@yandex.ru>
2020-04-11 16:56:15 +0400

    release 1.0.0.0

---
+++ ПростойШифровальщик/App.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+namespace ПростойШифровальщик
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        //https://professorweb.ru/my/WPF/base_WPF/level5/5_12.php
+        //https://www.codeproject.com/Articles/36412/Drag-and-Drop-ListBox
+
+        public static Window window;
+
+        public App()
+        {
+            //отлавливаем непойманные исключения
+            DispatcherUnhandledException += (sender, e) =>
+            {
+                MessageBox.Show("Фатальное исключение пойманое App " + e.Exception.GetType().ToString() + " отключено " + string.Format("An error occured: {0}", e.Exception.Message) + @"\r\nDispatcherUnhandledExceptionEventArgs - " + e + @"\r\nsender - " + sender);
+                e.Handled = true;
+            };
+
+            //Проверяем обновление если есть интернет
+            (new System.Threading.Thread(delegate () {
+                try
+                {
+                    WebClient client = new WebClient() { Credentials = CredentialCache.DefaultNetworkCredentials };
+
+                    Version versionSite = new Version(client.DownloadString("https://kba696.github.io/SimpleCryptographer/version"));
+                    if (versionSite > Assembly.GetExecutingAssembly().GetName().Version)
+                    {
+                        if (MessageBox.Show("Доступна новая версия. Скачать её?", "Скачать новую версию???", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                        {
+                            OpenBrowser("https://yadi.sk/d/k3dFaqVrHHADeQ");
+                        }
+                    }
+                }
+                catch { }
+            })).Start();
+
+            //Запускаем саму программу
+            window = new View.MainWindow() { DataContext = new ViewModel.MainWindow() };
+            window.Show();
+        }
+
+        void OpenBrowser(string url)
+        {
+            try
+            {
+                Process.Start(url);
+            }
+            catch
+            {
+                // hack because of this: https://github.com/dotnet/corefx/issues/10361
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    url = url.Replace("&", "^&");
+                    Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    Process.Start("xdg-open", url);
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    Process.Start("open", url);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
---
+++ ПростойШифровальщик/ViewModel/SectionDocument.cs
@@ -0,0 +1,61 @@
+﻿using MVVM;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Input;
+using ПростойШифровальщик.Model;
+
+namespace ПростойШифровальщик.ViewModel
+{
+    class SectionDocument : NotificationObject
+    {
+        InformationGroup informationGroup;
+        ObservableCollection<ContentControl> SomeCollection;
+        List<InformationGroup> InformationGroup;
+        public SectionDocument(InformationGroup informationGroup, ObservableCollection<ContentControl> SomeCollection, List<InformationGroup> InformationGroup)
+        {
+            this.informationGroup = informationGroup;
+            this.SomeCollection = SomeCollection;
+            this.InformationGroup = InformationGroup;
+        }
+
+        public string Name
+        {
+            get { return informationGroup.Name; }
+            set
+            {
+                if (value == informationGroup.Name) return;
+                informationGroup.Name = value;
+                MainWindow.ChangedFile();
+                OnPropertyChanged();
+            }
+        }
+
+        public string Informations
+        {
+            get { return informationGroup.Informations; }
+            set
+            {
+                if (value == informationGroup.Informations) return;
+                informationGroup.Informations = value;
+                MainWindow.ChangedFile();
+                OnPropertyChanged();
+            }
+        }
+
+        ICommand _RemoteInformations;
+        public ICommand RemoteInformations
+        {
+            get
+            {
+                return _RemoteInformations ?? (_RemoteInformations = new RelayCommand<object>(a =>
+                {
+                    MainWindow.ChangedFile();
+                    SomeCollection.Remove(SomeCollection.FirstOrDefault(x => x.DataContext == this));
+                    InformationGroup.Remove(informationGroup);
+                }));
+            }
+        }
+    }
+}
